dcaf799b72542e5a5a56c92ac98169f2db3755b9
Lucca-mito <luccasukman@gmail.com>
2017-04-15 02:04:23 -0300

    Added UK accent; barely functional

--- script.js
+++ script.js
@@ -1,34 +1,77 @@
+function showLoader() {
+	$('#result').html('<div class=loader></div>');
+}
+
+function MURICAH() {
+	return $('#accent').text() == '🇺🇸';
+}
+
+function toggleAccent() {
+	if (MURICAH()) $('#accent').text('🇬🇧');
+	else $('#accent').text('🇺🇸');
+}
+
+function transleite(ipa) {
+	var chart = MURICAH() ? chartUS : chartUK;
+
+	// Because .replace() only removes the first ocurrence
+	for (var i in chart) ipa = ipa.split(i).join(chart[i]);
+
+	if (ipa[0] == "-") ipa = ipa.substr(1);
+
+	return ipa;
+}
+
 function request() {
 	var word = $('input').val();
 	showLoader();
 
-	$.ajax(
+	if (MURICAH()) $.ajax( // US accent with WordsAPI
 		'https://wordsapiv1.p.mashape.com/words/'+word,
 		{ // <settings>
 			headers: {
 				"X-Mashape-Key": "y4yiFb5hiimshOtLWnLenJh6uNunp1M9OJpjsnEp17XCjIu8Kl",
 				"Accept": "application/json"
 			},
-			success: onSuccess,
+			success: onSuccessUS,
+			error: onError
+		} // </settings>
+	);
+	else $.ajax( // UK accent with Yandex
+		'https://dictionary.yandex.net/api/v1/dicservice.json/lookup',
+		{ //<settings>
+			data: {
+				key: 'dict.1.1.20160525T021329Z.baec1a754da78aeb.1348bc386523323a7c54ba25e13951734a211b83',
+				text: word,
+				lang: 'en-en'
+			},
+			success: onSuccessUK,
 			error: onError
 		} // </settings>
 	);
 }
 
-function showLoader() {
-	$('#result').html('<div class=loader></div>');
-}
-
-function onSuccess(data) {
+function onSuccessUS(data) {
+	data = data.pronunciation.all || data.pronunciation; // WordsAPI is stupid and inconsistent
 	$('#result').html(
 		'<div class="alert alert-success alert-dismissable fade in animate-bottom">\
 			<a href="#" class="close" data-dismiss="alert">&times;</a>'+
-			transleite(data.pronunciation.all) +'\
+			transleite(data) +'\
 		</div>'
 	);
 }
 
-function onError(jqXHR, textStatus) {
+function onSuccessUK(data) {
+	data = data.def[0].ts;
+	$('#result').html(
+		'<div class="alert alert-success alert-dismissable fade in animate-bottom">\
+			<a href="#" class="close" data-dismiss="alert">&times;</a>'+
+			transleite(data) +'\
+		</div>'
+	);
+}
+
+function onError() {
 	var word = $('input').val();
 	$('#result').html(
 		'<div class="alert alert-danger alert-dismissable fade in animate-bottom">\
@@ -39,41 +82,7 @@ function onError(jqXHR, textStatus) {
 	);
 }
 
-function transleite(ipa) {
-	var chart = {
-		'_': ' ',
-		"'": '-',
-		',': ' ',
-		'ɑ': 'ó',
-		'ɑ': 'ó',
-		'æ': 'é',
-		'ɛ': 'é',
-		'ɪ': 'i',
-		'ɔ': 'ó',
-		'ʊ': 'u',
-		'ju': 'iú',
-		'j': 'i',
-		'ɜ': 'â',
-		'ə': 'a',
-		'ð': 'd',
-		'ʒ': 'j',
-		'hw': 'w',
-		'ŋ': 'ng',
-		'θ': 'th', // FIXME: italics?
-		'r': 'Я',
-		'x': 'rr',
-		'tʃ': 'tch',
-		'ʃ': 'x'
-	};
-
-	if (ipa[0] == "'") ipa = ipa.substr(1);
-
-	// Because .replace() only removes the first ocurrence
-	for (var i in chart) ipa = ipa.split(i).join(chart[i]);
-
-	return ipa;
-}
-
 $(document).ready(function() {
+	$('#accent').click(toggleAccent);
 	$('button').click(request);
 })
